feb39be95dda95e5708defdfe557615768cd9855
André Alves <andrenpalves@gmail.com>
2024-12-19 03:27:48 +0100

    day 02 - part 2

--- day_02/main.exs
+++ day_02/main.exs
@@ -18,17 +18,27 @@ defmodule Day02 do
         Enum.all?(diff, fn x -> x < 0 && x > -4 end) || Enum.all?(diff, fn x -> x > 0 && x < 4 end)
     end
 
-    def count_safe_reports(file_path) do
+    def is_safe_with_problem_dampener(report) do
+        #TO-DO: try without brute-force
+        combinations = report
+            |> Enum.with_index()
+            |> Enum.map(fn {_, index} -> List.delete_at(report, index) end)
+
+        Enum.any?(combinations, &is_safe(&1))
+    end
+
+    def count_safe(file_path) do
         split_and_parse(file_path)
             |> Enum.count(&is_safe(&1))
     end
 
-    def part_2(_) do
-        "TBD"
+    def count_safe_with_problem_dampener(file_path) do
+        split_and_parse(file_path)
+            |> Enum.count(fn x -> is_safe(x) || is_safe_with_problem_dampener(x) end)
     end
 end
 
-IO.puts("Part 1 - example: #{Day02.count_safe_reports("example.txt")}")
-IO.puts("Part 1 - input: #{Day02.count_safe_reports("input.txt")}")
-#IO.puts("Part 2 - example: #{Day02.part_2("example.txt")}")
-#IO.puts("Part 2 - input: #{Day02.part_2("input.txt")}")
+IO.puts("Part 1 - example: #{Day02.count_safe("example.txt")}")
+IO.puts("Part 1 - input: #{Day02.count_safe("input.txt")}")
+IO.puts("Part 2 - example: #{Day02.count_safe_with_problem_dampener("example.txt")}")
+IO.puts("Part 2 - input: #{Day02.count_safe_with_problem_dampener("input.txt")}")
